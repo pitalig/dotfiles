d9c134cf48b86e4338bc3999553f89b03ea67ab8
Gabriel Pitali <gabriel.pitali@nubank.com.br>
2020-04-17 00:29:21 -0300

    Keep just useful screen layouts

--- .screenlayout/work.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output eDP1 --off --output DP1 --off --output DP2 --primary --mode 2560x1440 --pos 0x709 --rotate normal --output HDMI1 --mode 2560x1440 --pos 2560x0 --rotate left --output HDMI2 --off --output VIRTUAL1 --off
